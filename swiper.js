bc43ffbba3d5d8eb6984512ba2f229557654d541
qayumjon-coder <atajanovqayumjonbek@gmail.com>
2026-06-06 21:46:54 +0500

    new changes: adding container, and updated existing layout by container

--- swiper.js
+++ swiper.js
@@ -1,21 +1,14 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  slidesPerView: 4,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
 });
