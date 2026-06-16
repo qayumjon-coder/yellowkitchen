a281c5d320e16f1d24e2a18b99d2bc0c3eb22c67
qayumjon-coder <atajanovqayumjonbek@gmail.com>
2026-06-16 11:16:28 +0500

    Cart completed

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function toggleSidebar(){
+const cart_btn = document.querySelector(".cart_btn");
+const cart = document.querySelector(".cart");
+
+cart_btn.addEventListener("click", () => {
+
+    cart.classList.toggle("show");
+
+    setTimeout(() => {
+        cart.classList.toggle("smooth");
+    }, 100);
+});
+
+function toggleSidebar() {
     const menuBtn = document.querySelector('.menu_btn');
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('active');
